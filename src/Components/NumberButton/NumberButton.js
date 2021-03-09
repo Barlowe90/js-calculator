8b0b1570e57a7ae92e626e317c1c7dfa5477fd6d
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-03-09 13:53:48 +0100

    Handle number display
    I can use the function provider and handle it

--- src/Components/NumberButton/NumberButton.js
+++ src/Components/NumberButton/NumberButton.js
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { NumberContext } from "../NumberProvider/NumberProvider";
+
 export default function NumberButton({ buttonValue, id }) {
+  const { handleSetDisplayValue } = useContext(NumberContext);
+
   return (
-    <button type="button" id={id}>
+    <button
+      type="button"
+      id={id}
+      onClick={() => handleSetDisplayValue(buttonValue)}
+    >
       {buttonValue}
     </button>
   );
